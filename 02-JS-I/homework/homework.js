6d812410e14364397044d88e1d2a3ef2a65fc00a
LorenzoVinkler <imvinkelriedlorenzo@gmail.com>
2021-12-22 15:47:42 -0300

    seguimos probando

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -29,30 +29,40 @@ function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
   
+  return str;
+
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+
+  return x + y;
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+
+  return y - x;
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+
+  return x * y;
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+
+  return x / y;
   
 }
 
@@ -60,6 +70,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if(x == y & y == x){
+    return true;
+  }else{
+    return false;
+  }
   
 }
 
---
+++ Resumen/testing.js
@@ -0,0 +1,13 @@
+//https://prep.soyhenry.com/02-JS-I/
+
+var x,y;
+
+
+
+
+function suma(x,y){
+    x+y==b;
+    return b;
+
+}
+
